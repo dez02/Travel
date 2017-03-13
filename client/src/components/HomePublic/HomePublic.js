4b5d93f38e3640bb6d5ef27160ac92ebda9b39d9
Gambadeur <blardonemathieu@yahoo.fr>
2017-03-13 12:34:23 +0100

    creation component HomePublic

---
+++ client/src/components/HomePublic/HomePublic.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import NavBar from './Navbar/NavBar.js'
+
+
+
+
+class HomePublic extends Component {
+  render() {
+    console.log('start app');
+    return (
+      <div className="HomePublic">
+
+        <NavBar/>
+        <h1>Heloiuiue</h1>
+
+      </div>
+    );
+  }
+}
+
+export default HomePublic;
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import HomePublic from'./HomePublic/HomePublic.js'
 
 
 export default class Index extends React.Component {
     render(){
         return(
             <div>
-                
+                <HomePublic/>
             </div>
         )
     }
