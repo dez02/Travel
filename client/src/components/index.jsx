c0fa2f4b85e8fed6f9b64926c1353cecaee480b6
Gambadeur <blardonemathieu@yahoo.fr>
2017-03-13 23:12:17 +0100

    reglage conflit HomePublic

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomePublic from'./HomePublic/General/HomePublic.js'
-import HomePublic from'./HomePublic/HomePublic.js'
 import Header from './UsersHome/Header/Header.jsx';
 import Cards from './UsersHome/Cards/Cards.jsx';
 
