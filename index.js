f39358882b4855a8b4f8b3a350c1325fede48923
PaulPourtout <paulpourtout@hotmail.com>
2017-03-13 10:00:31 +0100

    mongoose added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
+
 
 // start the server
 app.listen(3000, () => {
