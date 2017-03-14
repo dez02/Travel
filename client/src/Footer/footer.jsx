10dce9c621b7fffb84ad663e2ddf56be7008a83a
Muanda <malawi.muanda7@gmail.com>
2017-03-14 11:56:51 +0100

    ajouter footer

---
+++ client/src/Footer/footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {render} from "react-dom";
+import './Footer.scss';
+
+
+class footer extends React.Component {
+  render () {
+    return (
+      <div className="footer">
+          <div className="menu">
+          <ul>
+            <li> Text text</li>
+            <li> Text text</li>
+            <li> Text text</li>
+          </ul>
+
+          <ul>
+            <li> text text </li>
+            <li> text text </li>
+            <li>text text </li>
+          </ul>
+      </div>
+
+        <ul>
+          <li>Accueil</li>
+          <li>Carte</li>
+          <li>Contact</li>
+        </ul>
+
+        <ul>
+          <li>contact </li>
+          <li> number </li>
+          <li>text text text</li>
+        </ul>
+      <img src="google"/>  <div class="sous menu">
+
+        
+
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default footer;
