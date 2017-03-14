7aecc6230783ffbcec38cffdd2518e96d8612c78
Gambadeur <blardonemathieu@yahoo.fr>
2017-03-14 15:02:11 +0100

    reglage confilt json/usershome/index.jsx

--- client/src/components/UsersHome/Cards/Cards.jsx
+++ client/src/components/UsersHome/Cards/Cards.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// import'./cards.css';
+import'./cards.css';
 
 class Cards extends React.Component{
 	render(){
 		return(
 			<div className="cards">
-<<<<<<< HEAD
 			<div className="grid">
 				<div className="item img">card1</div>
 				<div className="item">card2</div>
@@ -14,11 +13,7 @@ class Cards extends React.Component{
 				<div className="item">card5</div>
 				<div className="item">card6</div>
 			</div>				
-=======
-				<h1>jhjhjh</h1>
 
-
->>>>>>> fcdcf72e87250b3aea3f879dd73ffa7bee557950
 			</div>
 		);
 	}
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import HomePublic from'./HomePublic/General/HomePublic.js';
-=======
-import HomePublic from './HomePublic/HomePublic.js'
 import HomePublic from'./HomePublic/General/HomePublic.js'
->>>>>>> 4e4cbff3346fd347a95163b8ceb8f3ce46ca3d97
 import Header from './UsersHome/Header/Header.jsx';
 import AddComponent from './UsersHome/AddComponent/AddComponent.jsx';
 import Cards from './UsersHome/Cards/Cards.jsx';
-import Footer from '/Footer/footer.jsx';
+import Footer from './Footer/footer.jsx';
 
 
 
