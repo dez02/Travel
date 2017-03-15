031b85ba44d3b2f5ebbfc2526bde0ded6b5eb91f
PaulPourtout <paulpourtout@hotmail.com>
2017-03-15 11:42:23 +0100

    add routes.js + clean package.json

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
+const apiRouter = require('./server/app/routes');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const User = require('./server/app/models/user');
-const Travel = require('./server/app/models/travel');
 const port = process.env.PORT || 8080; // Define server PORT
 
 // Link To Database
@@ -13,9 +11,6 @@ mongoose.connect('mongodb://admin:simplonco@ds129720.mlab.com:29720/travel');
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 
-// App config
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -24,70 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// =============================================================
-// ===================     API ROUTER    ======================
-// =============================================================
-
-// Create mini app for API
-apiRouter = express.Router();
-
-// Access to /api/
-apiRouter.get('/', (req, res) => {
-  res.json('Hello in my great API');
-});
-
-// Route to /api/users
-apiRouter.route('/users')
-// See all users
-  .get((req, res) => {
-  User.find({}, (err, users) => {
-    if (err) {
-      throw err;
-    }
-    res.json(users);
-  });
-})
-// Add new user
-  .post((req, res) => {
-  var user = new User;
-  user.name = req.body.name;
-  user.username = req.body.username;
-  user.password = req.body.password;
-  user.admin = false;
-
-  user.save(err => {
-    if (err) {
-      if (err.code == 11000) {
-        return res.json({success: false, message: "L'utilisateur existe déjà"});
-      } else {
-        return res.send(err);
-      }
-    }
-    res.json({message: "GG ! L'utilisateur est ajouté !"});
-  });
-});
-
-// See user profile, update or delete
-apiRouter.route('/user/:user_id').get((req, res) => {
-  User.findById(req.params.user_id, (err, user) => {
-    if (err) {
-      throw err;
-    }
-    res.json(user);
-  });
-});
-
-// Route tu /api/travels
-apiRouter.get('/travels', (req, res) => {
-  Travel.find({}, (err, travels) => {
-    if (err) {
-      throw err;
-    }
-    res.json(travels);
-  });
-});
-
-// Define static route to API
+// API ROUTER
 app.use('/api', apiRouter);
 
 // start the server
