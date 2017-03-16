0e1beebccd36d0101f1199a08ba68bbd5548797b
PaulPourtout <paulpourtout@hotmail.com>
2017-03-16 17:11:06 +0100

    recuperation user infos

--- client/src/components/HomePublic/NavBar/NavBar.js
+++ client/src/components/HomePublic/NavBar/NavBar.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './NavBar.css';
 
 class NavBar extends Component {
-constructor(props) {
-	super(props);
-	this.state = {user: []};
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: []
+    };
+	// console.log('hello navbar');
+  }
 
-componentDiMount() {
-	this.UserDisplay();
-}
+  componentDidMount() {
+	//   console.log('hello navbar mount');
+    return fetch('http://localhost:8080/api/users', {method: 'get'})
+	.then(result => result.json())
+	.then(users => {
+      const user = users[0];
+      return this.setState({user});
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
-UserDisplay() {
-	return $.getJSON('localhost:')
-}
-
-
-    // inserer fonction js avec syntaxe REACT
+  // inserer fonction js avec syntaxe REACT
   render() {
-
+  //   const userName = this.state.users.map((item, i) => {
+  //     return <div>
+  //       <p>firstname : {item.firstName}</p>
+  //     </div>
+  //   });
+  // console.log(this.state.users[0]);
+console.log('hello navbar render');
     return (
-    <div className="navbar">
-      <a href="">
-      <div className="navbar-right">
-          <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
-          <div><p>Sign-up/Log-in</p></div>
+      <div className="navbar">
+        <a href="">
+          <div className="navbar-right">
+            <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+            <div>
+              <p>{this.state.user.firstName}</p>
+            </div>
+          </div>
+        </a>
       </div>
-       </a>
-    </div>
 
     );
   }
 
-
 }
-
 
 export default NavBar;
