dff3bd5af6201cfb4b0491e74eb685a08356a646
deuto <dezangelique1@gmail.com>
2017-03-17 16:01:50 +0100

    modif syntaxe in sectionHome.js

--- client/src/components/HomePublic/SectionHome/SectionHome.js
+++ client/src/components/HomePublic/SectionHome/SectionHome.js
@@ -11,7 +11,7 @@ class SectionHome extends Component {
         <div className="SectionHome-description">
           <h2 className="SectionHome-title">Es-tu vraiment joueur quand il s'agit de partir en vacances?</h2>
           <p>Ici on te propose de <span>choisir à ta place</span> !</p>
-          <p>C'est simple, tu t'enregistres, tu achètes un ticket en nous précisant si tu préfère passer tes vacances à te faire masser, faire la fête à <span>Vegas</span>, te glisser dans la peau d' <span>Indiana Jones</span> ou t'immerger dans la vie des berger de la <span>Cordière des Andes</span>...Et tu nous laisse faire le reste !</p>
+          <p>C'est simple, tu t'enregistres, tu achètes un ticket en nous précisant si tu préfères passer tes vacances à te faire masser, faire la fête à <span>Vegas</span>, te glisser dans la peau d' <span>Indiana Jones</span> ou t'immerger dans la vie des berger de la <span>Cordière des Andes</span>...Et tu nous laisses faire le reste !</p>
         </div>
         <p className="SectionHome-subtitle">Tu pourrais tomber sur des voyages de ce type :</p>
         <div className="GroupHomeItem">
